ec59941b24bd89350c00ebe1bdba63f73e0c993c
sungmowee <sungmo2787@naver.com>
2024-09-24 17:01:45 +0900

    Create 04_example_3.ino

---
+++ 04_example_3.ino
@@ -0,0 +1,19 @@
+#define PIN_LED 13
+unsigned int count, toggle;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(115200);
+  Serial.println("Hello World!");
+  count = toggle = 0;
+  digitalWrite(PIN_LED, toggle);
+
+}
+
+void loop() {
+  Serial.println(++count);
+  toggle = !toggle;
+  digitalWrite(PIN_LED, toggle);
+  delay(1000);
+  
+}
