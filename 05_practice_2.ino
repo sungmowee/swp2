367b57f09f92c0035659090ded1554340f71f7bf
sungmowee <sungmo2787@naver.com>
2024-09-30 22:36:58 +0900

    Create 05_practice_2.ino

---
+++ 05_practice_2.ino
@@ -0,0 +1,21 @@
+#define PIN_LED 7 
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+}
+
+void loop() {
+
+  digitalWrite(PIN_LED,1); 
+  delay(1000);            
+  
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(PIN_LED,0);  
+    delay(100);                 
+    digitalWrite(PIN_LED,1); 
+    delay(100);                 
+  }
+  digitalWrite(PIN_LED,0);
+  while(1){
+    } 
+}
